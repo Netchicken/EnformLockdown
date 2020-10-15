3725ae21e977f610c5fc81e110a28cadd3fca310
Gary Dix <gary.d@atcltd.atc.org.nz>
2020-10-15 14:04:48 +1300

    CSS formatting

--- ChatSample/Hubs/ChatHub.cs
+++ ChatSample/Hubs/ChatHub.cs
@@ -7,7 +7,7 @@ namespace LockDown.Hubs
     {
         public async Task Send(string name, string message)
         {
-            // Call the broadcastMessage method to update clients.
+            // Call the broadcastMessage method to update clients. broadcastMessage is the name of a method defined on the client in JavaScript. Clients.All.SendAsync will send an asynchronous message to the broadcastMessage method on all connected clients. That message contains the name and message. We can send more...............
             await Clients.All.SendAsync("broadcastMessage", name, message);
         }
     }
