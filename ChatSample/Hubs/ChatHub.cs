aad05a91e5f46da4e632c0c2490df1372bdf33fe
Gary Dix <gary.d@atcltd.atc.org.nz>
2020-10-16 15:29:25 +1300

    added roomlockdown indicator

--- ChatSample/Hubs/ChatHub.cs
+++ ChatSample/Hubs/ChatHub.cs
@@ -5,10 +5,10 @@ namespace LockDown.Hubs
 {
     public class LockDownHub : Hub
     {
-        public async Task Send(string name, string message, string warning)
+        public async Task Send(string name, string message, string warning, string RoomLockdown)
         {
             // Call the broadcastMessage method to update clients. broadcastMessage is the name of a method defined on the client in JavaScript. Clients.All.SendAsync will send an asynchronous message to the broadcastMessage method on all connected clients. That message contains the name and message. We can send more...............
-            await Clients.All.SendAsync("broadcastMessage", name, message, warning);
+            await Clients.All.SendAsync("broadcastMessage", name, message, warning, RoomLockdown);
         }
     }
 }
